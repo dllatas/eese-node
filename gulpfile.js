86e6ed75796748a2c54481b5bce97c86cbc493d9
dllatas <danllatas@gmail.com>
2016-08-07 12:16:17 +0200

    Adding JSX to JS transformation on Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
+var babel = require('gulp-babel');
+
+
 
 gulp.task('default', ['browser-sync'], function () {
-  gulp.watch(['public/*.html'], reload);
+    gulp.watch(['public/*.html'], reload);
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
@@ -15,7 +18,7 @@ gulp.task('browser-sync', ['nodemon'], function() {
   });
 });
 
-gulp.task('nodemon', function(cb) {
+gulp.task('nodemon', ['babel-react'], function(cb) {
     var called = false;
     return nodemon({
         script: 'bin/www'
@@ -34,4 +37,12 @@ gulp.task('nodemon', function(cb) {
             reload({ stream:false });
         }, 1000);
     });
+});
+
+gulp.task('babel-react', function() {
+    return gulp.src('views/components/*.jsx')
+        .pipe(babel({
+            presets: ['react']
+        }))
+        .pipe(gulp.dest('views'));
 });
